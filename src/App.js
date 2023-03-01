6059494913fc8b7c6fd3fc9a8e94d663e29be445
aarminoe <aarminoe@gmail.com>
2023-02-28 19:36:50 -0600

    installed react-router-dom added Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from './Header';
+import AboutMe from './AboutMe';
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={ <Home /> }/>
+      </Routes>
       <div><Header /></div>
+      <div><AboutMe /></div>
       Hello!
     </div>
   );
