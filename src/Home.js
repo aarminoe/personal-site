7d7d6a739edb3092415b55c347ffbe7839e805a9
aarminoe <aarminoe@gmail.com>
2023-03-05 14:12:44 -0600

    added css to home page

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AboutMe from "./AboutMe";
 import { Link } from "react-router-dom";
+import { Card } from "@mui/material";
 
 function Home() {
     return(
@@ -15,15 +16,22 @@ function Home() {
                 <div>
                     <AboutMe />
                 </div>
-                <div>
+                <Card className="tech-certificate-section">
                     <h3>Technical Certifications</h3>
                     <img src="https://aaron-noe-personal-site.s3.amazonaws.com/aws-certified-solutions-architect-associate.png"/>
-                    <Link to='https://www.credly.com/badges/239524d0-4c68-464a-bbb8-300e2845a15e'>Verify</Link>
+
                     <img src="https://aaron-noe-personal-site.s3.amazonaws.com/aws-certified-developer-associate+(1).png"/>
-                    <Link to='https://www.credly.com/badges/6b0f0cea-3ecb-4319-9ddc-66a24eb65aea'>Verify</Link>
+
                     <img src="https://aaron-noe-personal-site.s3.amazonaws.com/aws-certified-cloud-practitioner+(1).png"/>
-                    <Link to='https://www.credly.com/badges/0a745b40-a5c4-4b11-9200-8e8c525ebdd5'>Verify</Link>
-                </div>
+
+                    <div>
+                        <p>
+                            <Link className="verify-badge"   to='https://www.credly.com/badges/239524d0-4c68-464a-bbb8-300e2845a15e'>Verify</Link>
+                            <Link className="verify-badge"  to='https://www.credly.com/badges/6b0f0cea-3ecb-4319-9ddc-66a24eb65aea'>Verify</Link>
+                            <Link className="verify-badge" to='https://www.credly.com/badges/0a745b40-a5c4-4b11-9200-8e8c525ebdd5'>Verify</Link>
+                        </p>
+                    </div>
+                </Card>
             </div>
         </div>
     )
