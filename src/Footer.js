99ec322d68dae6817a905d7e5ed1a46c7b20810c
aarminoe <aarminoe@gmail.com>
2023-03-07 18:23:45 -0600

    added resume to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Bottom() {
     return(
         <div>
             <div className="contact-info">
-            | ✉️ aarminoe@gmail.com | ☏ 847-924-6488 |
+            | ✉️ aarminoe@gmail.com | ☏ 847-924-6488 | <Link to={"https://aaron-noe-personal-site.s3.amazonaws.com/Aaron+Noe+Resume+(1)+(1).pdf"}>Resume</Link> |
             </div>
+            
         </div>
     )
 }
