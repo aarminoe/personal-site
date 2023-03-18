75dcfcfedddb6bce34f04ec52b16d45d7d58d4f1
aarminoe <aarminoe@gmail.com>
2023-03-18 17:13:20 -0500

    added css to skills title

--- src/Skills.js
+++ src/Skills.js
@@ -3,7 +3,7 @@ import React from "react";
 function Skills() {
     return(
         <div>
-            <p>General Skills:</p>
+            <div className="skills-title">General Skills:</div>
             <div className="skills">
                 JavaScript, React, Python, AWS Lambda, AWS API Gateway, AWS EC2, AWS S3, AWS DynamoDB, AWS Cognito, AWS Amplify, AWS IAM, Relational Databases, CI/CD, SQL, MySQL, PostgreSQL, 
             </div>
