bb03d0704ed919f8f39a5285870830e9a01890bf
aarminoe <aarminoe@gmail.com>
2023-04-04 15:12:08 -0500

    terraform file updated

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+//When pushing to github run command (git filter-branch -f --index-filter 'git rm --cached -r --ignore-unmatch .terraform/')
+//after you must run terraform init again in order for terraform apply to work
+
+
 terraform {
   required_providers {
     aws = {
@@ -14,9 +18,30 @@ provider "aws" {
 
 resource "aws_s3_bucket" "aaron-noe-portfolio-site-personal" {
   bucket = "aaron-noe-portfolio-site-personal"
+  website {
+    index_document = "index.html"
+  }
+  policy = jsonencode({
+    "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": "s3:GetObject",
+          "Resource": "arn:aws:s3:::aaron-noe-portfolio-site-personal/*"
+        }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_acl" "aaron-noe-portfolio-site-personal-acl" {
   bucket = aws_s3_bucket.aaron-noe-portfolio-site-personal.id   
   acl = "public-read"
+}
+
+resource "aws_s3_bucket_object" "object1" {
+  bucket = aws_s3_bucket.aaron-noe-portfolio-site-personal.id
+  key = "obj-test"
+  acl = "public-read"
+  source = "C:\\Users\\aarmi\\Downloads\\favicon.ico"
+  etag = filemd5("C:\\Users\\aarmi\\Downloads\\favicon.ico")
 }
